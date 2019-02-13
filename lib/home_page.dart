92eddcee2e1792e210adde0a84630f00413995fe
tamura-a <tamura-a@mainichi.co.jp>
2019-02-13 14:24:20 +0900

    first commit.

---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(child: Text('統計で見る日本')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ), 
+            ListTile(
+              // https://www.e-stat.go.jp/stat-search/database?page=1&layout=datalist&toukei=00130001&tstat=000001103615&cycle=7&statdisp_id=0003194968&second=1&second2=1&tclass1val=0
+              title: Text('犯罪統計'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
